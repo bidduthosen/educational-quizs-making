804ec7526654707df243cbbb644bd0f704fa9852
bidduthosen <mdbidduthosen34@gmail.com>
2022-10-12 02:21:57 +0600

    dynamic options loaded and style

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -3,16 +3,17 @@ import './QuizDetails.css'
 
 const QuizDetails = ({quiz}) => {
     const {question,options}= quiz;
-    console.log(quiz)
+    // console.log(quiz);
+    const handleAddToCard = quiz =>{
+        console.log(quiz)
+    }
     return (
         <div className='quiz-info'>
             <h2>Question: {question}</h2>
-            <div className='quiz-question'>
-                <h6>{options[0]}</h6>
-                <h6>{options[1]}</h6>
-                <h6>{options[2]}</h6>
-                <h6>{options[3]}</h6>
-            </div>
+            
+            {
+                options.map((option, index )=> <div className='quiz-question' key={index} onClick={()=>handleAddToCard(quiz)}>{option}</div>)
+            }
         </div>
     );
 };
