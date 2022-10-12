a95eb022c7d77975a7042c2d40624a8ef026c46d
bidduthosen <mdbidduthosen34@gmail.com>
2022-10-12 15:14:06 +0600

    quiz current answare find in alart and style css added

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuizQuestion from '../QuizQuestion/QuizQuestion';
 import './QuizDetails.css'
 
 const QuizDetails = ({quiz}) => {
-    const {question,options}= quiz;
+    const {question,options,correctAnswer}= quiz;
     // console.log(quiz);
     const handleAddToCard = quiz =>{
-        console.log(quiz)
+        // console.log(quiz)
+        const exist =correctAnswer;
+        if(exist === quiz){
+            alert('right');
+        }
+        else{
+            alert('worng')
+        }
+
     }
     return (
         <div className='quiz-info'>
-            <h2>Question: {question}</h2>
-            
+            <h4>Question: {question}</h4>
+            <div className='quiz-question'>
             {
-                options.map((option, index )=> <div className='quiz-question' key={index} onClick={()=>handleAddToCard(option)}>{option}</div>)
+                options.map((option, index )=> <QuizQuestion key={index} handleAddToCard={handleAddToCard} option={option}></QuizQuestion>)
             }
+            </div>
         </div>
     );
 };
---
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './QuizQuestion.css'
+
+const QuizQuestion = ({option,handleAddToCard}) => {
+    return (
+        <div onClick={()=>handleAddToCard(option)} className='quiz-ques-info'>
+            <h6>{option}</h6>
+        </div>
+    );
+};
+
+export default QuizQuestion;
